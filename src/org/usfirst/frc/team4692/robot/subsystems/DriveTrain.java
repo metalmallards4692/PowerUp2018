41e43a19b4d1ae90b0d0c8d11e6c97129937db82
JHL100 <verllee46@gmail.com>
2018-02-13 18:44:48 -0800

    Changed our TalonSRX import
    To use WPI_TalonSRX and it still works.

--- src/org/usfirst/frc/team4692/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team4692/robot/subsystems/DriveTrain.java
@@ -5,21 +5,21 @@ import org.usfirst.frc.team4692.robot.RobotMap;
 import org.usfirst.frc.team4692.robot.commands.TankDrive;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class DriveTrain extends Subsystem {
-	private TalonSRX LeftMotor;
-	private TalonSRX LeftMotorFollower;
-	private TalonSRX RightMotor;
-	private TalonSRX RightMotorFollower;
+	private WPI_TalonSRX LeftMotor;
+	private WPI_TalonSRX LeftMotorFollower;
+	private WPI_TalonSRX RightMotor;
+	private WPI_TalonSRX RightMotorFollower;
 
 	public DriveTrain() {
-		LeftMotor = new TalonSRX(RobotMap.LEFTMOTOR.value);
-		RightMotor = new TalonSRX(RobotMap.RIGHTMOTOR.value);
-		LeftMotorFollower = new TalonSRX(RobotMap.LEFT_FOLLOW_MOTOR.value);
-		RightMotorFollower = new TalonSRX(RobotMap.RIGHT_FOLLOW_MOTOR.value);
+		LeftMotor = new WPI_TalonSRX(RobotMap.LEFTMOTOR.value);
+		RightMotor = new WPI_TalonSRX(RobotMap.RIGHTMOTOR.value);
+		LeftMotorFollower = new WPI_TalonSRX(RobotMap.LEFT_FOLLOW_MOTOR.value);
+		RightMotorFollower = new WPI_TalonSRX(RobotMap.RIGHT_FOLLOW_MOTOR.value);
 		Robot.initTalon(LeftMotor);
 		Robot.initTalon(RightMotor);
 		Robot.initTalon(LeftMotorFollower);
